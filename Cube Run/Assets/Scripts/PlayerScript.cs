1dff45f9a426f007f8b42e00bb43074b10b220be
hammone2 <hammone2@my.erau.edu>
2024-02-29 20:27:13 -0700

    added more stuff

--- Cube Run/Assets/Scripts/PlayerScript.cs
+++ Cube Run/Assets/Scripts/PlayerScript.cs
@@ -9,27 +9,43 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce = 700f;
     public float sideForce = 500f;
+    private bool isAlive = true;
 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        if (Input.GetKey("d")) 
+        if (isAlive)
         {
-            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+
+            if (rb.position.y < -3f)
+            {
+                kill();
+            }
         }
+        
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle")
         {
-            enabled = false;
+            kill();
         }
+    }
+
+    void kill()
+    {
+        isAlive = false;
+        FindObjectOfType<GameManager>().EndGame();
     }
 }
